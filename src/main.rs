af9dc73af336401055cde314bcdcd97951bfdffb
tompko <tompko@gmail.com>
2016-03-02 20:02:29 +0000

    Parse args for perft file path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut perft_file_path = "".to_string();
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Arendil chess engine");
+        ap.refer(&mut perft_file_path)
+            .add_option(&["-p", "--perft"], Store,
+            "Path to perft file");
+        ap.parse_args_or_exit();
+    }
+
+    if perft_file_path != "" {
+        println!("{}", perft_file_path);
+    }
 }
